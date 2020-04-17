525f7db117eedda5b036dad549837bd1ced1312b
fi3te <fi3te@users.noreply.github.com>
2020-04-17 15:33:40 +0200

    Media queries added, refactoring

--- src/views/List.vue
+++ src/views/List.vue
@@ -37,3 +37,64 @@ export default class List extends Vue {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+pre {
+  background-color: unset;
+  font-family: unset;
+  margin: 0;
+  padding: 0;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+}
+
+tr {
+  border-bottom: 1px solid var(--color-lightGrey);
+
+  td {
+    min-width: 100px;
+  }
+}
+
+@media (max-width: 600px) {
+  table,
+  tbody,
+  tr,
+  th,
+  td {
+    display: block;
+  }
+
+  thead {
+    display: none;
+  }
+
+  tr {
+    border-bottom: 2px solid var(--color-lightGrey);
+
+    td {
+      padding-left: 30%;
+    }
+
+    td:before {
+      position: absolute;
+      left: 15px;
+      width: 30%;
+      font-weight: bold;
+    }
+
+    td:nth-of-type(1):before {
+      content: "Name";
+    }
+    td:nth-of-type(2):before {
+      content: "Description";
+    }
+    td:nth-of-type(3):before {
+      content: "Date";
+    }
+    td:nth-of-type(4):before {
+      content: "End date";
+    }
+  }
+}
+</style>
